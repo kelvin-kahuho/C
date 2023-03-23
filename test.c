1c066ad51b4b732090e9420a142c1f814a2f8b10
ke-stacks <kingkevo254@gmail.com>
2023-03-23 12:22:24 +0300

    More basic programs using C

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, non-zero value on error
+ */
+int main(void)
+{
+	int i = 10;
+	char name = "Meg";
+
+	/* Function to print */
+	printf("%c is %d old", name, i);
+	return (0);
+}
