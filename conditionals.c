f66057cbaab1b3e52e5c6825d8a2e6b11fe1e182
ke-stacks <kingkevo254@gmail.com>
2023-03-24 11:15:01 +0300

    C program that implements if else statement

---
+++ conditionals.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int x = 20;
+int y = 21;
+
+if (x > y)
+{
+printf("%d is less than %d", x, y);
+}
+else
+{
+printf("%d is greator than %d", y, x);
+}
+
+return 0;
+}
--- name.c
+++ name.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main()
 {
-  char name[16];
+  char name[60];
   int age;
 
   printf("Enter your name: \n");
   scanf("%s", name);
 
+  string second_name = get_string("Enter second name: ");
+
   printf("Enter your age: \n");
   scanf("%d", &age);
 
-  printf("Your name is %s and you are %d years old.\n", name, age);
+  printf("Your name is %s %s and you are %d years old.\n", name, second_name, age);
 
   return 0;
 }
