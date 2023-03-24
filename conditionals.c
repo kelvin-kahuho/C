7efd6b9d3b0923f58e9975c33b00f66882483a01
ke-stacks <kingkevo254@gmail.com>
2023-03-24 11:33:33 +0300

    Modification of C program that implements if else statement

--- conditionals.c
+++ conditionals.c
@@ -2,16 +2,26 @@
 
 int main()
 {
-int x = 20;
-int y = 21;
+int x;
+int y;
+
+printf("Enter X: \n");
+scanf("%d", &x);
+
+printf("Enter Y: \n");
+scanf("%d", &y);
 
 if (x > y)
 {
 printf("%d is less than %d", x, y);
 }
-else
+else if (x < y)
 {
 printf("%d is greator than %d", y, x);
+}
+else
+{
+printf("%d is equal to %d", x, y);
 }
 
 return 0;
