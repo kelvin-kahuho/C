5c50324b2f1ba0c02ff00cb2b21614b869dca76b
ke-stacks <kingkevo254@gmail.com>
2023-03-26 17:28:26 +0300

    While loop in C

---
+++ whileloop.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+main - entry point of the program
+
+return 0
+*/
+
+int main()
+{
+  int i = 0;
+  while (i <= 20)
+  {
+    printf("%d", i);
+    i++;
+  }
+
+  return 0;
+}
